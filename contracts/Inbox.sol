49571a326bb4bb10e233599bcf8498b4df9d24eb
martin0044 <martin0044@gmail.com>
2021-11-25 19:42:50 +0000

    Testing getAccounts

--- contracts/Inbox.sol
+++ contracts/Inbox.sol
@@ -1,4 +1,18 @@
 pragma solidity ^0.4.17;
-// linter warnings (red underline) about pragma version can igonored!
 
+contract Inbox {
+    string public message;
+    
+    function Inbox(string initalMessage) public{
+        message = initalMessage;
+    }
+    
+    function setMessage(string newMessage) public{
+        message = newMessage;
+    }
+    
+    function getMessage() public view returns (string) {
+        return message;
+    }
+}
 // contract code will go here
--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -1,1 +1,20 @@
 // contract test code will go here
+const assert = require('assert')
+const ganache = require('ganache-cli')
+const Web3 = require('web3')
+const web3 = new Web3(ganache.provider())
+const {interface, bytecode} = require('../compile')
+
+let accounts;
+
+beforeEach(async () => {
+    accounts = await web3.eth.getAccounts()
+    
+})
+
+describe('Inbox', () => {
+
+    it('Deploy contract',() => {
+        console.log(accounts)
+    })
+})
